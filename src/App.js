09115eb94e82a483ab38700f8785a458c6ca0096
hammadnaveed12 <hammadnaveed010@gmail.com>
2024-03-07 02:54:52 +0500

    Revert "Merge branch 'master' of https://github.com/hammadnaveed12/hifi"
    This reverts commit c1f1dc622c345ca791b64d56a03a6f95d5dc2d1e, reversing
    changes made to 94d9940d7b8ba2a53a7cc6f672f1c12c9b4458c4.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import { DarkModeProvider } from './components/darkmode';
+import Home from './screens/Home';
+
+function App() {
+  return (
+  
+      <div className="App">
+        <Home />
+      </div>
+    
+  );
+}
+
+export default App;
